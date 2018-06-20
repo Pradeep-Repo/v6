90626cac5a3594496d0a732e24494dd9de516c6b
Pradeep <er.spider29@gmail.com>
2018-06-21 02:31:12 +0530

    base

---
+++ src/main/java/com/example/v6/V6Application.java
@@ -0,0 +1,12 @@
+package com.example.v6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(V6Application.class, args);
+    }
+}
